5d7580f16db674d285e3e2a24c942014a5a8198a
starzyk.bartlomiej <starzyk.bartlomiej@gmail.com>
2023-04-22 18:25:56 +0200

    poprawki

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 # URLConf
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='core/store.html')),
     path('products/', views.product_list),
     path('products/<int:id>/', views.product_detail),
     path('products/delete/<int:id>/', views.product_delete),
