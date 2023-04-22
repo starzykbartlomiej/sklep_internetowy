c851ea66ef46366321daa3a0c8520e4c1ced62f1
starzyk.bartlomiej <starzyk.bartlomiej@gmail.com>
2023-04-22 19:57:25 +0200

    kolejne zmiany

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from . import views
 # URLConf
 urlpatterns = [
     # path('', views.say_hello)
-    path('', TemplateView.as_view(template_name='core/index.html'))
+    path('', TemplateView.as_view(template_name='core/index.html')),
+    path('project', TemplateView.as_view(template_name='core/project.html'))
 ]
